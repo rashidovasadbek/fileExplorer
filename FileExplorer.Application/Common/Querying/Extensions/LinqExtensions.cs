a1cbfd846027dcbb68996f1ed8a3ef7e96aca191
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-08 22:38:56 +0500

    Adedd same files

---
+++ FileExplorer.Application/Common/Querying/Extensions/LinqExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FileExplorer.Applicatoin.Common.Filtering;
+
+namespace FileExplorer.Applicatoin.Common.Querying.Extensions;
+
+public static class LinqExtensions
+{
+    public static IQueryable<TSource> ApplyPagination<TSource>(this IQueryable<TSource> source,
+        FilterPagination paginationOptions)
+    {
+        return source.Skip((int)((paginationOptions.PageToken - 1) * paginationOptions.PageSize))
+            .Take((int)paginationOptions.PageSize);
+    }
+
+    public static IEnumerable<TSource> ApplyPagination<TSource>(this IEnumerable<TSource> source,
+        FilterPagination paginationOptions)
+    {
+        return source.Skip((int)((paginationOptions.PageToken - 1) * paginationOptions.PageSize))
+            .Take((int)paginationOptions.PageSize);
+    }
+}
---
+++ FileExplorer.Application/FIleStorege/Services/IFileService.cs
@@ -0,0 +1,17 @@
+﻿using FileExplorer.Applicatoin.Common.Filtering;
+using FileExplorer.Applicatoin.FIleStorege.Models.Filtering;
+using FileExplorer.Applicatoin.FIleStorege.Models.Storage;
+
+namespace FileExplorer.Applicatoin.FIleStorege.Services;
+
+public interface IFileService
+{
+    ValueTask<IList<StorageFile>> GetFilesByPathAsync(IEnumerable<string> filesPath);
+
+    ValueTask<StorageFile> GetFileByPathAsync(string filePath);
+
+    IEnumerable<StorageFilesSummary> getFilesSummary(IEnumerable<StorageFile> files);
+
+    StorageFileType GetFileType(string filePath);
+
+}

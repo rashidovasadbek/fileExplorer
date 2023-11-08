c059250a4f2bf28f1ea1eaa6d6b188baf1246f28
Asadbek Rashidov <rashidovasadbek264@gmail.com>
2023-11-08 22:38:01 +0500

    Added Broker

---
+++ FileExplorer.Infrastructure/FileStorage/Brokers/DriveBroker.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using FileExplorer.Applicatoin.FIleStorege.Broker;
+using FileExplorer.Applicatoin.FIleStorege.Models;
+using FileExplorer.Applicatoin.FIleStorege.Models.Storage;
+
+namespace FileExplorer.Infrastructure.FileStorage.Brokers;
+
+public class DriveBroker : IDriveBroker
+{
+    private readonly IMapper _mapper;
+
+    public DriveBroker(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public IEnumerable<StorageDrive> Get()
+    {
+        return DriveInfo
+            .GetDrives()
+            .Select(driveInfo => _mapper.Map<StorageDrive>(driveInfo))
+            .AsQueryable();
+
+    }
+}
---
+++ FileExplorer.Infrastructure/FileStorage/Brokers/FileBroker.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using FileExplorer.Applicatoin.FIleStorege.Broker;
+using FileExplorer.Applicatoin.FIleStorege.Models.Storage;
+
+namespace FileExplorer.Infrastructure.FileStorage.Brokers;
+
+public class FileBroker : IFileBroker
+{
+    private readonly IMapper _mapper;
+
+    public FileBroker(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+    public StorageFile GetByPath(string filePath)
+    {
+        return _mapper.Map<StorageFile>(new FileInfo(filePath));
+    }
+}
